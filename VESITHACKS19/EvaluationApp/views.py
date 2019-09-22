dcb33ceb46951e259ba16bb2312d8ac4d210d907
Shubham Darekar <2017.shubham.darekar@ves.ac.in>
2019-09-22 19:24:45 +0530

    DOne

--- VESITHACKS19/EvaluationApp/views.py
+++ VESITHACKS19/EvaluationApp/views.py
@@ -5,14 +5,40 @@ from django.shortcuts import render
 from django.template import loader
 #from django.template import context
 #import mysql.connector
+from EvaluationApp.forms import LoginForm
+from .models import User
 
 def index(request):
-	return render(request,"EvaluationApp/index.html")
+	return render(request,"EvaluationApp/landing page.html")
 # Create your views here.
 
 def Login(request):
-	return render(request,"EvaluationApp/employeedash.html")
+	return render(request,"EvaluationApp/loginpage.html")
 
 def openadmin(request):
 	return render(request,"EvaluationApp/admin-dashboard.html")
+
+def openemp(request):
+	return render(request,"EvaluationApp/employeedash.html")
+
+def opendept(request):
+	return render(request,"EvaluationApp/dept-dashboard.html")
+
+def openrpt(request):
+	return render(request,"EvaluationApp/reportform.html")
+
+def authlogin(request):
+	if request.method =='POST':
+		details = LoginForm(request.POST)
+		if (details.is_valid()):
+			return redirect('EvaluationApp/landing page.html')
+		users = User.objects.get.all()
+		for user in users:
+			if details.user == user.email and details.password == user.password:
+				if user.role == "admin":
+					return HttpResponse("<h1>Hii admin</h1>",csrfContext)
+
+		
+		return HttpResponse("<h1>Failed</h1>",)		
+		
 

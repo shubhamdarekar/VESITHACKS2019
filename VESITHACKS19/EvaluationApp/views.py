b069e8f412a46a97ff86a9b08fdae60561d878cc
Narayani <2017.narayani.patil@ves.ac.in>
2019-09-22 15:22:33 +0530

    Redirect to index pagr

--- VESITHACKS19/EvaluationApp/views.py
+++ VESITHACKS19/EvaluationApp/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+#from django.db import connection
+#from .models import EvaluationApp
+from django.template import loader
+#from django.template import context
+#import mysql.connector
 
+def index(request):
+	return render(request,"EvaluationApp/index.html")
 # Create your views here.
